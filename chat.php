7de0636083d2c913a6e1fa9a628d08a7c2449e0c
asterjdm <114984907+asterjdm@users.noreply.github.com>
2023-01-21 14:53:25 +0100

    Add files via upload

--- chat.php
+++ chat.php
@@ -52,60 +52,57 @@ function containsBadWord($string)
 
 session_start();
 include_once("db.php");
-if(isset($_SESSION["user"]) == FALSE)
+
+
+
+
+
+if(isset($_POST["text"]))
 {
-    header('Location: Connexion.php');
-}
-
-
-
-
-    if(isset($_POST["text"]))
-    {
-        
-        
-        $text = $conn -> real_escape_string(htmlspecialchars($_POST["text"]));
-        $user = $_SESSION['userPK'];
-       
-
-        if($text !== "")
-        {
-            if(containsBadWord(strtolower($_POST["text"])) == FALSE){
-                
-                $sql = "INSERT INTO ju_chat (content, USER_FK) VALUES ('$text', '$user')";
-            
-
-
-
-
-                if (mysqli_query($conn, $sql)) {
-
-                    $sqlL = "SELECT * FROM ju_chat";
-                    $nbChat = $conn->query($sqlL)->num_rows;
-
-                    if ($nbChat > $CHAT_LIMIT) {
-                        $sqlD = "DELETE FROM ju_chat LIMIT 1";
-                        mysqli_query($conn, $sqlD);
-                    }
-                    
-                    $sqlM = "SELECT mail FROM ju_Users WHERE mail_new_chat = 'on'";
-                    $resultM = $conn->query($sqlM);
-                    
-                    while($row = $resultM->fetch_assoc()) {
-                        mail($row["mail"], $_SESSION['user']." a envoyé un message dans le chat !", "Voila le message de ".$_SESSION['user']." : ".$text." \n\n  Allez sur https://rmbi.ch/cescosite/chat.php pour lui répondre !");
-                    }
-                    header('Location: chat.php');
-        
-                }else{
-                    echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
-                }
     
+    
+    $text = $conn -> real_escape_string(htmlspecialchars($_POST["text"]));
+    $user = $_SESSION['userPK'];
+    
+
+    if($text !== "")
+    {
+        if(containsBadWord(strtolower($_POST["text"])) == FALSE){
+            
+            $sql = "INSERT INTO ju_chat (content, USER_FK) VALUES ('$text', '$user')";
+        
+
+
+
+
+            if (mysqli_query($conn, $sql)) {
+
+                $sqlL = "SELECT * FROM ju_chat";
+                $nbChat = $conn->query($sqlL)->num_rows;
+
+                if ($nbChat > $CHAT_LIMIT) {
+                    $sqlD = "DELETE FROM ju_chat LIMIT 1";
+                    mysqli_query($conn, $sqlD);
+                }
+                
+                $sqlM = "SELECT mail FROM ju_Users WHERE mail_new_chat = 'on'";
+                $resultM = $conn->query($sqlM);
+                
+                while($row = $resultM->fetch_assoc()) {
+                    mail($row["mail"], $_SESSION['user']." a envoyé un message dans le chat !", "Voila le message de ".$_SESSION['user']." : ".$text." \n\n  Allez sur https://rmbi.ch/cescosite/chat.php pour lui répondre !");
+                }
+                header('Location: chat.php');
+    
+            }else{
+                echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
             }
 
         }
 
-
     }
+
+
+}
 ?>
 
 
@@ -134,6 +131,10 @@ if(isset($_SESSION["user"]) == FALSE)
 
 
 <script src="./chat.js"></script>
+
+<footer>
+    <input id ='chatData' type="text">
+</footer>
 
 </body>
 
