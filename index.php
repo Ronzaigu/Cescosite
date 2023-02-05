637d0b5f37c084e9d31e4ee1087cfc2aa4ac32ac
judemont <julien@rmbi.ch>
2023-02-05 17:51:09 +0100

    42

--- index.php
+++ index.php
@@ -21,11 +21,7 @@
         <link rel="stylesheet" href="trumbowyg/dist/plugins/highlight/ui/trumbowyg.highlight.min.css">
 
         <!--bootstrap library:-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"crossorigin="anonymous">
-
-        <!--bootstrap library:-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"crossorigin="anonymous"></script>
-
+    
 </head>
 
 <body>
@@ -49,7 +45,7 @@
                 if (isset($_SESSION["user"])) {
                         echo '<button onclick=window.location.href="./deconect.php"; class="special"><p class="subtitle">Déconnexion</p></button>';
                 } else {
-                        echo '<button onclick=window.location.href="?page=connection""; class="special"><p class="subtitle">Connexion</p></button>';
+                        echo '<button onclick=window.location.href="?page=connection"; class="special"><p class="subtitle">Connexion</p></button>';
                         echo '<button onclick=window.location.href="?page=inscription"; class="navigator"><p class="subtitle">Inscription</p></button>';
                 }
                 ?>
