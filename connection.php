8c4bf6fa772937f25d5b592fdf40eb1a0fe48225
AstroidusTv <asteroidus@protonmail.com>
2023-03-11 15:23:55 +0100

    new style for connection and inscription

---
+++ connection.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="./css/connection.css">
+	<title>Connexion</title>
+</head>
+<body>
+
+	<div class="connection">
+		
+		<h1 class="connection_h1">CONNEXION</h1>
+		<div class="inputs">
+			<p class="username_text" style="margin-top: 75">Nom d'utilisateur</p>
+			<div class="line"></div>
+			<input type="text" name="Nom d'utilisateur" placeholder="Nom d'utilisateur" class="username_input">
+			<p class="password_text">Mot de passe</p>
+			<div class="line"></div>
+			<input type="password" name="Mot de passe" placeholder="Mot de passe" class="password_input">
+		</div>
+		<div class="buttons">
+		<button class="connection_button">Connexion</button>
+		</div>
+		<a class="no_account" href="?page=inscription"><p>Pas de compte ?</p></a>
+
+	</div>
+
+</body>
+</html>
